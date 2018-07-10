736a6358c51abdb48e1b40960a9e52512a87c2fb
kurniawanhandito <kurniawanhandito@gmail.com>
2018-07-10 13:53:32 +0700

    learn how to use numerical non-dec data type

---
+++ src/belajar/chap9-datatype.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	//numerical non-decimal data type
+	var positiveNumber uint8 = 89
+	var negativeNumber = -124234123
+	fmt.Printf("bilangan positif: %d\n", positiveNumber)
+	fmt.Printf("bilangan negatif: %d\n", negativeNumber)
+}
