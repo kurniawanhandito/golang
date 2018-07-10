0fb6144faaab8722c75787abb53d65794a2a3bde
kurniawanhandito <kurniawanhandito@gmail.com>
2018-07-10 11:46:07 +0700

    multiline and inline commentary

--- src/belajar/chap7-commentary.go
+++ src/belajar/chap7-commentary.go
@@ -3,8 +3,16 @@ package main
 import "fmt"
 
 func main() {
+	// inline commentary
 	// commentary code
 	// show hello world message
+
+	/*
+		multiline commentary
+		commentary code
+		show hello world message
+	*/
+
 	fmt.Println("hello world")
 
 	// fmt.println("this line will not shown")
