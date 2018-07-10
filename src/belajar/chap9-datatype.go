70f8672a786dd27a71e489d64d8ff8a860d948a5
kurniawanhandito <kurniawanhandito@gmail.com>
2018-07-10 13:54:59 +0700

    learn how to use numerical decimal data type

--- src/belajar/chap9-datatype.go
+++ src/belajar/chap9-datatype.go
@@ -8,4 +8,9 @@ func main() {
 	var negativeNumber = -124234123
 	fmt.Printf("bilangan positif: %d\n", positiveNumber)
 	fmt.Printf("bilangan negatif: %d\n", negativeNumber)
+
+	//numericl decimal data type
+	var decimalNumber = 2.62
+	fmt.Printf("bilangan desimal: %f\n", decimalNumber)
+	fmt.Printf("bilangan desimal: %.3f\n", decimalNumber)
 }
