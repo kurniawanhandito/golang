fe94122d45f486ba48ed3a578f5c8541c976028b
kurniawanhandito <kurniawanhandito@gmail.com>
2018-07-10 11:59:38 +0700

    learn how to declare variable with data type

--- src/belajar/chap8-variable.go
+++ src/belajar/chap8-variable.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-
+	//varable with data type
+	var firstName string = "John"
+	var lastName string
+	lastName = "Wick"
+	fmt.Printf("halo %s %s!\n", firstName, lastName)
 }
