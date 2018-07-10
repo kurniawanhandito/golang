dac3b03205154b00a42a346ed23cd3d6d66d7f3a
kurniawanhandito <kurniawanhandito@gmail.com>
2018-07-10 13:18:59 +0700

    learn how to declare multi-variable

--- src/belajar/chap8-variable.go
+++ src/belajar/chap8-variable.go
@@ -13,4 +13,15 @@ func main() {
 	var secondName string = "John"
 	thirdName := "Wick"
 	fmt.Println("halo", secondName, thirdName, "!")
+
+	//declare multi variable
+	var first, second, third string
+	first, second, third = "satu", "dua", "tiga"
+	fmt.Println(first, second, third)
+	var fourth, fifth, sixth string = "empat", "lima", "enam"
+	fmt.Println(fourth, fifth, sixth)
+	seventh, eight, ninth := "tujuh", "delapan", " sembilan"
+	fmt.Println(seventh, eight, ninth)
+	one, isTuesday, twoPointTwo, say := 1, true, 2.2, "hello"
+	fmt.Println(one, isTuesday, twoPointTwo, say)
 }
